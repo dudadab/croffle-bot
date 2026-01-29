ee7489ca06bfc2c9e18d9694425c3a04296b6f54
KimJiHun <kmr2397@naver.com>
2026-01-30 01:43:33 +0900

    feat: Clear 기능

---
+++ src/commands/music/clear.ts
@@ -0,0 +1,25 @@
+import { ApplyOptions } from '@sapphire/decorators';
+import { Command } from '@sapphire/framework';
+import { useQueue } from 'discord-player';
+import type { Message } from 'discord.js';
+
+@ApplyOptions<Command.Options>({
+	description: '현재 대기열의 모든 곡을 삭제합니다.',
+	aliases: ['c']
+})
+export class UserCommand extends Command {
+	public override async messageRun(message: Message) {
+		if (!message.guildId) return;
+
+		const queue = useQueue(message.guildId);
+
+		if (!queue || queue.tracks.size === 0) {
+			await message.reply('❌ 현재 대기열에 곡이 없습니다.');
+			return;
+		}
+
+		queue.tracks.clear();
+		await message.reply(`✅ 대기열이 비워졌습니다.`);
+		return;
+	}
+}
