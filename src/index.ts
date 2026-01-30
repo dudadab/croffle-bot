dbc9c964a949f1fcdec04b9b18a06dfcf97ba661
BlueNyang <xpsj20@gmail.com>
2026-01-30 20:57:55 +0900

    refactor: generate with po token enable

--- src/index.ts
+++ src/index.ts
@@ -50,7 +50,11 @@ export class CustomClient extends SapphireClient {
 	public override async login(token?: string) {
 		await this.player.extractors.loadMulti(DefaultExtractors);
 		await this.player.extractors.register(YoutubeiExtractor, {
-			cookie: process.env.YOUTUBE_COOKIE || ''
+			cookie: process.env.YOUTUBE_COOKIE || '',
+			generateWithPoToken: true,
+			streamOptions: {
+				useClient: 'ANDROID'
+			}
 		});
 
 		const loaded = this.player.extractors.store.size;
